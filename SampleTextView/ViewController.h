1b06106ca3b23d80f5cc92c7b343680bafd2326d
ei17ringo <ei17ringo@gmail.com>
2015-01-09 13:05:01 +0800

    Initial Commit

---
+++ SampleTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleTextView
+//
+//  Created by Eriko Ichinohe on 2015/01/09.
+//  Copyright (c) 2015年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
